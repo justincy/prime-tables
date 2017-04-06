6e59297f34e7b59a2f4f3a6db64f652952b6fee7
Justin <justincyork@gmail.com>
2017-04-06 19:34:54 +0000

    validated prime number generator

--- test/primes.js
+++ test/primes.js
@@ -1,14 +1,27 @@
 const primes = require('../lib/primes');
 const assert = require('chai').assert;
+const primesList = require('./primes.json');
 
 describe('primes', function(){
   
   it('5 primes', function(){
-    assert.deepEqual(primes(5), [2, 3, 5, 7, 11]);
+    assert.deepEqual(primes(5), primesList.slice(0, 5));
   });
   
   it('10 primes', function(){
-    assert.deepEqual(primes(10), [2, 3, 5, 7, 11, 13, 17, 19, 23,	29]);
+    assert.deepEqual(primes(10), primesList.slice(0, 10));
+  });
+  
+  it('100 primes', function(){
+    assert.deepEqual(primes(100), primesList.slice(0, 100));
+  });
+  
+  it('1000 primes', function(){
+    assert.deepEqual(primes(1000), primesList.slice(0, 1000));
+  });
+  
+  it('10000 primes', function(){
+    assert.deepEqual(primes(10000), primesList);
   });
   
 });
