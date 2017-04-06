e02295c2008e9b35619e84ddb48e8f7243ded14a
Justin <justincyork@gmail.com>
2017-04-06 20:44:21 +0000

    move printing into it's own module

--- prime-tables.js
+++ prime-tables.js
@@ -1,6 +1,6 @@
 var argv = require('minimist')(process.argv.slice(2));
 var generatePrimes = require('./lib/primes');
-var bignum = require('bignum');
+var printTable = require('./lib/printTable');
 
 var n = parseInt(argv._[0], 10);
 if(!Number.isInteger(n)){
@@ -8,35 +8,4 @@ if(!Number.isInteger(n)){
   process.exit(1);
 }
 
-var primes = generatePrimes(n);
-
-// Naive table generation. i = row; j = column
-for(var i = 0; i <= n; i++){
-  
-  // Start the line
-  process.stdout.write('|');
-  
-  for(var j = 0; j <= n; j++){
-    
-    // Empty square in the top-left corner
-    if(i === 0 && j === 0){
-      process.stdout.write(' ');
-    } 
-    
-    // Show the prime number on the top and left edges
-    else if(i === 0 || j === 0){
-      process.stdout.write(primes[(i || j) - 1] + '');
-    }
-    
-    // Show the multiple
-    else {
-      process.stdout.write(bignum.mul(primes[i-1], primes[j-1]).toString());
-    }
-    
-    // Close the cell
-    process.stdout.write('|');
-  }
-  
-  // End the line
-  process.stdout.write('\n');
-}
+printTable(generatePrimes(n));
